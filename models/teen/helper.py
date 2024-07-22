ebf4ad0d3bb60695a8b79e17c9ccffd223638aa4
27220 <2722044837@qq.com>
2024-07-22 21:14:25 +0800

    调参

--- models/teen/helper.py
+++ models/teen/helper.py
@@ -17,10 +17,11 @@ def base_train(model, trainloader, optimizer, scheduler, epoch, args, fc):
         logits = model(data)
         logits = logits[:, :args.base_class]
 
-        proto_0 = fc.weight[:, 0]
-        dists = F.cosine_similarity(fc.weight[:, 1:], proto_0.unsqueeze(1), dim=0)
+        # proto_0 = fc.weight[:, 0]
+        # dists = F.cosine_similarity(fc.weight[:, 1:], proto_0.unsqueeze(1), dim=0)
 
-        loss = F.cross_entropy(logits, train_label) + dists.mean()
+        # loss = F.cross_entropy(logits, train_label) + dists.mean()
+        loss = F.cross_entropy(logits, train_label)
         # loss = margin_loss(logits, train_label)
         acc = count_acc(logits, train_label)
 
